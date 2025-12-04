d65db86864e683d043513039942445eb4a094188
jpenic <jp010809267@fer.hr>
2025-12-04 09:15:24 +0100

    Day 04

---
+++ day_04/day_04.py
@@ -0,0 +1,25 @@
+with open('input.txt', 'r') as f:
+    lines = f.read().split()
+
+grid_dict = {}
+
+# using complex numbers for the grid coordinates
+for row in range(len(lines)):
+    for column in range(len(lines[row])):
+        grid_dict[row * 1j + column] = lines[row][column]
+
+movable_rolls = 0
+
+positions = grid_dict.keys()
+for position in positions:
+    if grid_dict.get(position) == '.':
+        continue
+    counter = 0
+    for move in (1, -1, 1j, -1j,  1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j):
+        if grid_dict.get(position + move) == '@':
+            counter += 1
+    if counter < 4:
+        movable_rolls += 1
+
+print("Result:", movable_rolls)
+
